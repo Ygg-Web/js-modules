e758671ba605a589e22c3c3d95ed3769e5b23967
Nikita <TsarevNF@yandex.ru>
2021-10-11 21:29:30 +0300

    changing the module structure

--- timer/script.js
+++ timer/script.js
@@ -1,23 +1,18 @@
-let date = new Date(' Jan 1 2022 00:00:00');
+let timeOfAction = ' Jan 1 2022 00:00:00';
 
-const counts = () => {
-    let now = new Date();
-    gap = date - now;
+const getTimeRemaining = (endtime) => {
+    let period = new Date(endtime) - new Date(),
+        days = Math.floor(period / 1000 / 60 / 60 / 24),
+        hours = Math.floor(period / 1000 / 60 / 60) % 24,
+        minutes = Math.floor(period / 1000 / 60) % 60,
+        seconds = Math.floor(period / 1000) % 60;
 
-    let days = Math.floor(gap / 1000 / 60 / 60 / 24),
-        hours = Math.floor(gap / 1000 / 60 / 60) % 24,
-        minutes = Math.floor(gap / 1000 / 60) % 60,
-        seconds = Math.floor(gap / 1000) % 60;
-
-    if (gap < 0) {
-
-        document.querySelector('#group').innerText = 'Время вышло';
-
-    } else {
-        document.getElementById('d').innerText = getZero(days);
-        document.getElementById('h').innerText = getZero(hours);
-        document.getElementById('m').innerText = getZero(minutes);
-        document.getElementById('s').innerText = getZero(seconds);
+    return {
+        'period': period,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
     }
 }
 
@@ -29,6 +24,27 @@ const getZero = (num) => {
     }
 }
 
-counts();
+const setTimer = (selector, endtime) => {
+    const timer = document.querySelector(selector),
+        days = document.querySelector('#d'),
+        hours = document.querySelector('#h'),
+        minutes = document.querySelector('#m'),
+        seconds = document.querySelector('#s'),
+        timeInterval = setInterval(updateClock, 1000);
 
-setInterval(counts, 1000);
+
+}
+
+// if (period < 0) {
+
+//     document.querySelector('#group').innerText = 'Время вышло';
+
+// } else {
+//     document.getElementById('d').innerText = getZero(days);
+//     document.getElementById('h').innerText = getZero(hours);
+//     document.getElementById('m').innerText = getZero(minutes);
+//     document.getElementById('s').innerText = getZero(seconds);
+// }
+getTimeRemaining(timeOfAction);
+
+// setInterval(getTimeRemaining, 1000);
