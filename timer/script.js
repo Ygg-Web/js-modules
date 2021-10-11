9883233904fd1f91075eb95a39ede69d1a59437a
Nikita <TsarevNF@yandex.ru>
2021-10-11 21:07:52 +0300

    added a zero substitution function in the range from 0 to 10

--- timer/script.js
+++ timer/script.js
@@ -4,23 +4,29 @@ const counts = () => {
     let now = new Date();
     gap = date - now;
 
-    let days = Math.floor(gap / 1000 / 60 / 60 / 24);
-    let hours = Math.floor(gap / 1000 / 60 / 60) % 24;
-    let minutes = Math.floor(gap / 1000 / 60) % 60;
-    let seconds = Math.floor(gap / 1000) % 60;
+    let days = Math.floor(gap / 1000 / 60 / 60 / 24),
+        hours = Math.floor(gap / 1000 / 60 / 60) % 24,
+        minutes = Math.floor(gap / 1000 / 60) % 60,
+        seconds = Math.floor(gap / 1000) % 60;
 
     if (gap < 0) {
 
         document.querySelector('#group').innerText = 'Время вышло';
 
     } else {
-        document.getElementById('d').innerText = days;
-        document.getElementById('h').innerText = hours;
-        document.getElementById('m').innerText = minutes;
-        document.getElementById('s').innerText = seconds;
+        document.getElementById('d').innerText = getZero(days);
+        document.getElementById('h').innerText = getZero(hours);
+        document.getElementById('m').innerText = getZero(minutes);
+        document.getElementById('s').innerText = getZero(seconds);
     }
+}
 
-
+const getZero = (num) => {
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
 }
 
 counts();
