8ebae1d2d73d7a9d6565ff8e9a8f9f8c754dd61e
Nikita <TsarevNF@yandex.ru>
2021-10-21 21:46:13 +0300

    code optimization, use of arrow functions in drag in drop module

--- dragInDrop/script.js
+++ dragInDrop/script.js
@@ -1,45 +1,18 @@
 const item = document.querySelector('.item')
 const placeholders = document.querySelectorAll('.placeholder')
 
-item.addEventListener('dragstart', dragstart)
-item.addEventListener('dragend', dragend)
-
-for (const placeholder of placeholders) {
-    placeholder.addEventListener('dragover', dragover)
-    placeholder.addEventListener('dragenter', dragenter)
-    placeholder.addEventListener('dragleave', dragleave)
-    placeholder.addEventListener('drop', dragdrop)
-}
-
-
-function dragstart(e) {
+item.addEventListener('dragstart', (e) => {
     e.target.classList.add('hold')
     setTimeout(() => e.target.classList.add('hide'), 0)
-}
+})
+item.addEventListener('dragend', (e) => e.target.classList.remove('hold', 'hide'))
 
-function dragend(e) {
-    e.target.classList.remove('hold', 'hide')
-}
-
-function dragover(e) {
-    e.preventDefault()
-}
-
-
-function dragenter(e) {
-    console.log('dragenter')
-    e.target.classList.add('hovered')
-
-}
-
-function dragleave(e) {
-    e.target.classList.remove('hovered')
-
-}
-
-function dragdrop(e) {
-    e.target.classList.remove('hovered')
-
-    e.target.append(item)
-
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', (e) => e.preventDefault())
+    placeholder.addEventListener('dragenter', (e) => e.target.classList.add('hovered'))
+    placeholder.addEventListener('dragleave', (e) => e.target.classList.remove('hovered'))
+    placeholder.addEventListener('drop', (e) => {
+        e.target.classList.remove('hovered')
+        e.target.append(item)
+    })
 }
