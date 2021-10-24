f866788330a65d1844e8b685c1a51f8d4b2edcdd
Nikita <TsarevNF@yandex.ru>
2021-10-24 14:14:41 +0300

    added function getRandomColor for circle

--- aim_game/script.js
+++ aim_game/script.js
@@ -2,7 +2,8 @@ const startBtn = document.querySelector('#start'),
     screens = document.querySelectorAll('.screen'),
     timeList = document.querySelector('#time-list'),
     timeEl = document.querySelector('#time'),
-    board = document.querySelector('#board')
+    board = document.querySelector('#board'),
+    colors = ['#FF6347', '#DC143C', '#B8860B', '#CD853F', '#778899', '#2F4F4F', '#FAEBD7', '#7B68EE', '#191970', '#7CFC00']
 
 let time = 0,
     score = 0
@@ -58,6 +59,7 @@ function finishGame() {
 
 function createRandomCircle() {
     const circle = document.createElement('div'),
+        color = getRandomColor(),
         size = getRandomNumber(10, 60),
         { width, height } = board.getBoundingClientRect(),
         x = getRandomNumber(0, width - size),
@@ -68,10 +70,15 @@ function createRandomCircle() {
     circle.style.height = `${size}px`
     circle.style.top = `${y}px`
     circle.style.left = `${x}px`
+    circle.style.backgroundColor = color
 
     board.append(circle)
 }
 
 function getRandomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
 }
