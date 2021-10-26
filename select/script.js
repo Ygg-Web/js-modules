bbbafcf66d2012441b08820760a7ee273db493cb
Nikita <TsarevNF@yandex.ru>
2021-10-26 23:52:06 +0300

    added functions setup, clickHandler, toggle and getter isOpen

--- select/script.js
+++ select/script.js
@@ -1,6 +1,6 @@
 const getTemplate = () => {
     return `
-    <div class="select__input">
+    <div class="select__input" data-type="input">
     <span>Hello</span>
     <i class="fa fa-chevron-down" aria-hidden="true"></i></div>
     <div class="select__dropdown">
@@ -21,11 +21,30 @@ class Select {
         this.elem = document.querySelector(selector)
 
         this.render()
+        this.setup()
     }
 
     render() {
         this.elem.classList.add('select')
         this.elem.innerHTML = getTemplate()
+    }
+    setup() {
+        this.clickHandler = this.clickHandler.bind(this)
+        this.elem.addEventListener('click', this.clickHandler)
+    }
+
+    clickHandler(event) {
+        const { type } = event.target.dataset
+        if (type === 'input') {
+            this.toggle()
+        }
+    }
+    get isOpen() {
+        return this.elem.classList.contains('open')
+    }
+
+    toggle() {
+        this.isOpen ? this.close() : this.open()
     }
 
     open() {
@@ -34,6 +53,10 @@ class Select {
 
     close() {
         this.elem.classList.remove('open')
+    }
+
+    destroy() {
+        this.elem.removeEventListener('click', this.clickHandler)
     }
 }
 
