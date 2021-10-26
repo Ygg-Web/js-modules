3077472c577926eeac55333ba3f870e30fd18b8a
Nikita <TsarevNF@yandex.ru>
2021-10-26 23:35:54 +0300

    added functions open, close, render for select JS

--- select/script.js
+++ select/script.js
@@ -1,7 +1,45 @@
-class Select() {
-    constructor() {
+const getTemplate = () => {
+    return `
+    <div class="select__input">
+    <span>Hello</span>
+    <i class="fa fa-chevron-down" aria-hidden="true"></i></div>
+    <div class="select__dropdown">
+    <ul class="select__list">
+        <li class="select__item">123</li>
+        <li class="select__item">123 </li>
+        <li class="select__item">123 </li>
+        <li class="select__item">123 </li>
+        <li class="select__item">123 </li>
+        <li class="select__item">123 </li>
 
+    </ul>
+</div>`
+}
+
+class Select {
+    constructor(selector, options) {
+        this.elem = document.querySelector(selector)
+
+        this.render()
+    }
+
+    render() {
+        this.elem.classList.add('select')
+        this.elem.innerHTML = getTemplate()
+    }
+
+    open() {
+        this.elem.classList.add('open')
+    }
+
+    close() {
+        this.elem.classList.remove('open')
     }
 }
 
-const select = new Select()
+
+const select = new Select('#select', {
+
+})
+
+window.s = select
